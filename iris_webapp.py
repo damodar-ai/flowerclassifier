33b6912fd9c18d0d1a6f9881d58c08d5ecb63718
DAMODAR TIWARI <117446972+Damodar12345678@users.noreply.github.com>
2023-10-31 22:28:48 +0530

    Add files via upload

---
+++ iris_webapp.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pickle
+
+
+
+lin_model=pickle.load(open('lin_model.pkl','rb'))
+log_model=pickle.load(open('log_model.pkl','rb'))
+svm=pickle.load(open('svm.pkl','rb'))
+
+def classify(num):
+    if num<0.5:
+        return 'Setosa'
+    elif num <1.5:
+        return 'Versicolor'
+    else:
+        return 'Virginica'
+def main():
+    st.title("Flowers Classifier")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px; border-radius:50px;">
+    <h2 style="color:white;text-align:center;">Iris Classification</h2>
+    </div>
+    <div style="background-color:yellow; padding-top:10px; margin:10px;
+     border-radius:50px;">
+    <h6 style="color:black;text-align:center;">Project By:-Damodar Tiwari</h6>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    activities=['Linear Regression','Logistic Regression','SVM']
+    option=st.sidebar.selectbox('Which model would you like to use?',activities)
+    st.subheader(option)
+    sl=st.slider('Select Sepal Length', 0.0, 10.0)
+    sw=st.slider('Select Sepal Width', 0.0, 10.0)
+    pl=st.slider('Select Petal Length', 0.0, 10.0)
+    pw=st.slider('Select Petal Width', 0.0, 10.0)
+    inputs=[[sl,sw,pl,pw]]
+    if st.button('Classify'):
+        if option=='Linear Regression':
+            st.success(classify(lin_model.predict(inputs)))
+        elif option=='Logistic Regression':
+            st.success(classify(log_model.predict(inputs)))
+        else:
+           st.success('the ',classify(svm.predict(inputs)))
+
+
+if __name__=='__main__':
+    main()
+
+st.markdown('Damodar Tiwari')
